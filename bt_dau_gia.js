ad93586fb50435626e25221dc7ab48080c8f8f59
chinh <chinh000529@gmail.com>
2021-12-16 14:19:04 +0700

    done

--- bt_dau_gia.js
+++ bt_dau_gia.js
@@ -1,33 +1,29 @@
 const stepPrices1 = {
-    100: 40,
-    300: 60,
-    500: 80,
-    1000: 120,
-    1500: 150,
-}
+  100: 40,
+  300: 60,
+  500: 80,
+  1000: 120,
+  1500: 150,
+};
 
 function calcMinPrice(price, stepPrices, rivalryPrice) {
-    const keys = Object.keys(stepPrices);
-    const values = Object.values(stepPrices);
-    let status = 0; //quy định A trả giá trước
-    while (price <= rivalryPrice) {
-        for (let i = 0; i < keys.length; i++) {
-            if (price <= +keys[i] || (price >= +keys[i] && price < +keys[i + 1]) || price >= +keys[keys.length - 1]) {
-                if (status % 2 === 0) {
-                    if (price + values[i] <= rivalryPrice) {
-                        price += values[i];
-                        status += 1;
-                        break;
-                    } else return price;
-                } else {
-                    price += values[i];
-                    status += 1;
-                    break;
-                }
-            }
-        }
+  const keys = Object.keys(stepPrices);
+  const values = Object.values(stepPrices);
+  while (price <= rivalryPrice) {
+    for (let i = 0; i < keys.length; i++) {
+      if (
+        price <= +keys[i] ||
+        (price >= +keys[i] && price < +keys[i + 1]) ||
+        price >= +keys[keys.length - 1]
+      ) {
+        if (price + values[i] <= rivalryPrice) {
+          price += values[i];
+          break;
+        } else return price + values[i];
+      }
     }
-    return price;
+  }
+  return price;
 }
 
 console.log(calcMinPrice(210, stepPrices1, 1200));
